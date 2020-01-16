2e5bacca244f3ecbaaafcc31635dae3df515d2de
Logan Negley <logannegley@gmail.com>
2020-01-15 19:19:41 -0600

    added link for movie card

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
-
 import SavedList from './Movies/SavedList';
+import {Route} from 'react-router-dom';
+import MovieList from '../src/Movies/MovieList';
+import Movie from '../src/Movies/Movie'
+
+
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -10,10 +14,18 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className='App'>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      {/* Routes */}
+      <Route exact path='/'>
+        <MovieList/>
+      </Route>
+      <Route path='/movies:id'>
+        <Movie/>
+      </Route>
     </div>
+
+
   );
 };
 
