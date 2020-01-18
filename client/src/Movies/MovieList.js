b99c7eb0e148961cb4002669d073b5c111232b14
Logan Negley <logannegley@gmail.com>
2020-01-17 18:45:41 -0600

    chnaged passed props to curly brackets

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 import {NavLink} from 'react-router-dom';
+import MovieCard from '../Movies/MovieCard';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -34,23 +35,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <Link to={`/movies/${movie.id}`}>
-    {/* {console.log(movie)} */}
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+    <MovieCard title={title} director={director} metascore={metascore} stars={stars}/>
     </Link>
   );
 }
